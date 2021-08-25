361403e2c60cc874607a28d400b3e1b87d065f1a
Snehitha-23 <katakamsnehitha@gmail.com>
2021-08-25 13:40:41 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Header from './Components/Header';
+import Content from './Components/Content'
+import './Components/App.scss';
+const App = props => {
+    return (
+        <>
+           <Header/>
+           <Content/>
+        </>
+    )
+}
+
+export default App;
---
+++ src/Components/Header.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+// import logo from '../assets/images/logo.png';
+const Header = props => {
+    return (
+    <header className="header">
+        <nav>
+            <div className="logo">
+               
+            </div>
+        </nav>
+
+    </header>
+    )
+}
+
+export default Header
